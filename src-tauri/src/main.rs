11fdfbe94eca38b366b5325147ec78b94dff4a29
Eric <eric.schmale@gmx.de>
2023-06-19 20:52:07 +0200

    create table

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use rusqlite::{params, Connection, Result};
+
 mod auftrag;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -33,4 +35,43 @@ fn main() {
         .invoke_handler(tauri::generate_handler![greet, get_position])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+fn create_tables() {
+    let conn = Connection::open("./auftrag.db").unwrap();
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS auftrag (
+            id INTEGER PRIMARY KEY,
+            auftragsnummer INTEGER NOT NULL,
+            kunde TEXT NOT NULL,
+            auftragspositionen TEXT NOT NULL,
+            kopftext TEXT NOT NULL,
+            besteller TEXT NOT NULL
+        )",
+        params![],
+    )
+    .unwrap();
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS auftragsposition (
+            id INTEGER PRIMARY KEY,
+            auftragid INTEGER NOT NULL,
+            position INTEGER NOT NULL,
+            artikel TEXT NOT NULL,
+            menge INTEGER NOT NULL,
+            preisid INTEGER NOT NULL
+        )",
+        params![],
+    )
+    .unwrap();
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS preis (
+            id INTEGER PRIMARY KEY,
+            position INTEGER NOT NULL,
+            netto REAL NOT NULL,
+            brutto REAL NOT NULL,
+            mwst REAL NOT NULL
+        )",
+        params![],
+    )
+    .unwrap();
 }
